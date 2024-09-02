d39fa08c34ce68d75f8a370e970da2ad4d8ca38f
BrooklynIT <Tech@brooklynitsolution.com>
2024-09-02 13:44:47 -0400

    Revert "Merge pull request #2 from KelvinTegelaar/main"
    This reverts commit db9b0b9032fa25036a9b818ff9f8a7128bcccd4f, reversing
    changes made to 4afa234495117316f40a8bcb0df4c6040bf8f9d0.

--- src/views/identity/administration/Users.jsx
+++ src/views/identity/administration/Users.jsx
@@ -275,8 +275,7 @@ const Offcanvas = (row, rowIndex, formatExtraData) => {
             color: 'info',
             modal: true,
             modalUrl: `/api/ExecResetPass?MustChange=true&TenantFilter=${tenant.defaultDomainName}&ID=${row.id}&displayName=${row.displayName}`,
-            modalMessage:
-              'Are you sure you want to reset the password for this user? The user must change their password at next logon.',
+            modalMessage: 'Are you sure you want to reset the password for this user?',
           },
           {
             label: 'Reset Password',
@@ -286,11 +285,11 @@ const Offcanvas = (row, rowIndex, formatExtraData) => {
             modalMessage: 'Are you sure you want to reset the password for this user?',
           },
           {
-            label: 'Pre-provision OneDrive',
+            label: 'Preprovision OneDrive',
             color: 'info',
             modal: true,
             modalUrl: `/api/ExecOneDriveProvision?TenantFilter=${tenant.defaultDomainName}&UserPrincipalName=${row.userPrincipalName}`,
-            modalMessage: 'Are you sure you want to pre-provision OneDrive for this user??',
+            modalMessage: 'Are you sure you want to preprovision onedrive for this user??',
           },
           {
             label: 'Clear ImmutableId',
